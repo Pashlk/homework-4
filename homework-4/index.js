8da7d78acd62d68007ede43b0ae9ccf46c0ab102
Pashlk <102326168+Pashlk@users.noreply.github.com>
2022-03-27 12:13:41 +0300

    Add files via upload

---
+++ homework-4/index.js
@@ -0,0 +1,158 @@
+function sum(a, b) {
+    return a + b;
+}
+
+const sumFunction = function (a, b) {
+    return a + b;
+};
+
+const arrowSum = (a, b) => {
+    return a + b;
+};
+
+function sub(a, b) {
+    return a - b;
+}
+
+const subFunction = function (a, b) {
+    return a - b;
+};
+
+const arrowSub = (a, b) => {
+    return a - b;
+};
+
+function mul(a, b) {
+    return a * b;
+}
+
+const mulFunction = function (a, b) {
+    return a * b;
+};
+
+const arrowMul = (a, b) => {
+    return a * b;
+};
+
+function div(a, b) {
+    return a / b;
+}
+
+const divFunction = function (a, b) {
+    return a / b;
+};
+
+const arrowDiv = (a, b) => {
+    return a / b;
+};
+
+function mod(a, b) {
+    return a % b;
+}
+
+const modFunction = function (a, b) {
+    return a % b;
+};
+
+const arrowMod = (a, b) => {
+    return a % b;
+};
+
+function larger(a, b) {
+    return a > b;
+}
+
+const largerFunction = function (a, b) {
+    return a > b;
+};
+
+const arrowLarger = (a, b) => {
+    return a > b;
+};
+
+function less(a, b) {
+    return a < b;
+}
+
+const lessFunction = function (a, b) {
+    return a < b;
+};
+
+const arrowLess = (a, b) => {
+    return a < b;
+};
+
+function largerOrEqual(a, b) {
+    return a >= b;
+}
+
+const largerOrEqualFunction = function (a, b) {
+    return a >= b;
+};
+
+const arrowLargerOrEqual = (a, b) => {
+    return a >= b;
+};
+
+function lessOrEqual(a, b) {
+    return a <= b;
+}
+
+const lessOrEqualFunction = function (a, b) {
+    return a <= b;
+};
+
+const arrowLessOrEqual = (a, b) => {
+    return a <= b;
+};
+
+function equal(a, b) {
+    return a === b;
+}
+
+const equalFunction = function (a, b) {
+    return a === b;
+};
+
+const arrowEqual = (a, b) => {
+    return a === b;
+};
+
+function unequal(a, b) {
+    return a !== b;
+}
+
+const unequalFunction = function (a, b) {
+    return a !== b;
+};
+
+const arrowUnequal = (a, b) => {
+    return a !== b;
+};
+
+console.log('Вызов функции sum:', sum(2, 2));
+
+console.log('Вызов функции sub:', sub(2, 2));
+
+console.log('Вызов функции mul:', mul(2, 2));
+
+console.log('Вызов функции div:', div(2, 2));
+
+console.log('Вызов функции mod:', mod(2, 2));
+
+console.log('Вызов функции larger:', larger(2, 2));
+
+console.log('Вызов функции less:', less(2, 2));
+
+console.log('Вызов функции largerOrEqual:', largerOrEqual(2, 2));
+
+console.log('Вызов функции lessOrEqual:', lessOrEqual(2, 2));
+
+console.log('Вызов функции equal:', equal(2, 2));
+
+console.log('Вызов функции unequal:', unequal(2, 2));
+
+
+
+
+
